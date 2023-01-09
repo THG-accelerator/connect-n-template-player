2948d6ac6aa6cf7ee847757b97c826a338971ce4
Alan Kerby <Alan.Kerby40@thehutgroup.com>
2023-01-09 14:47:57 +0000

    four vs random

--- src/main/java/com/thg/accelerator23/connectn/ai/halninethousand/random1.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/halninethousand/random1.java
@@ -5,16 +5,16 @@ import com.thehutgroup.accelerator.connectn.player.Counter;
 import com.thehutgroup.accelerator.connectn.player.Player;
 
 
-public class halNineThousandConnector extends Player {
-  public halNineThousandConnector(Counter counter) {
+public class random1 extends Player {
+  public random1(Counter counter) {
     //TODO: fill in your name here
-    super(counter, halNineThousandConnector.class.getName());
+    super(counter, random1.class.getName());
   }
 
   @Override
   public int makeMove(Board board) {
     //TODO: some crazy analysis
     //TODO: make sure said analysis uses less than 2G of heap and returns within 10 seconds on whichever machine is running it
-    return 4;
+    return (int) (Math.random() * 10);
   }
 }
