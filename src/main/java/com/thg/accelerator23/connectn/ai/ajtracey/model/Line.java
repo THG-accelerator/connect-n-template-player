ae3cdaf8a0f33977ac1ceb9771304a96035001ac
JoeMcD <joseph.mcdonagh64@thehutgroup.com>
2023-01-09 15:44:14 +0000

    Random AI built, ready to publish version 1.0

---
+++ src/main/java/com/thg/accelerator23/connectn/ai/ajtracey/model/Line.java
@@ -0,0 +1,8 @@
+package com.thg.accelerator23.connectn.ai.ajtracey.model;
+
+import com.thehutgroup.accelerator.connectn.player.Counter;
+
+import java.util.Iterator;
+
+public interface Line extends Iterator<Counter> {
+}
