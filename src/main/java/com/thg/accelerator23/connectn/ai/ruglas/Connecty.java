6a24f6b92bd10434b4fd9f980fb7e90456786f62
Ruchika Scott <ru.scott24@gmail.com>
2023-01-09 11:26:42 +0000

    refactored for our team

--- src/main/java/com/thg/accelerator23/connectn/ai/ruglas/Connecty.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/ruglas/Connecty.java
@@ -1,14 +1,14 @@
-package com.thg.accelerator23.connectn.ai.yourteam;
+package com.thg.accelerator23.connectn.ai.ruglas;
 
 import com.thehutgroup.accelerator.connectn.player.Board;
 import com.thehutgroup.accelerator.connectn.player.Counter;
 import com.thehutgroup.accelerator.connectn.player.Player;
 
 
-public class NameAi extends Player {
-  public NameAi(Counter counter) {
+public class Connecty extends Player {
+  public Connecty(Counter counter) {
     //TODO: fill in your name here
-    super(counter, NameAi.class.getName());
+    super(counter, Connecty.class.getName());
   }
 
   @Override
