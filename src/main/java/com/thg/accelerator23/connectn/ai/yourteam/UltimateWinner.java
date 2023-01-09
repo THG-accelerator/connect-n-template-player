a50dcb0bc15f532b24c96cfc301a6754bfb5d66b
InesCortina <Ines.Cortina91@thehutgroup.com>
2023-01-09 11:25:42 +0000

    First changes to pom and naming

--- src/main/java/com/thg/accelerator23/connectn/ai/yourteam/UltimateWinner.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/yourteam/UltimateWinner.java
@@ -1,14 +1,14 @@
-package com.thg.accelerator23.connectn.ai.yourteam;
+package com.thg.accelerator23.connectn.ai.ultimatewinner;
 
 import com.thehutgroup.accelerator.connectn.player.Board;
 import com.thehutgroup.accelerator.connectn.player.Counter;
 import com.thehutgroup.accelerator.connectn.player.Player;
 
 
-public class NameAi extends Player {
-  public NameAi(Counter counter) {
+public class UltimateWinner extends Player {
+  public UltimateWinner(Counter counter) {
     //TODO: fill in your name here
-    super(counter, NameAi.class.getName());
+    super(counter, UltimateWinner.class.getName());
   }
 
   @Override
