15d843a388cc8afe68f1bdbefc8209669f969d64
john chen <john.chen86@thehutgroup.com>
2023-01-09 14:04:57 +0000

    Random move within the limits

--- src/main/java/com/thg/accelerator23/connectn/ai/fermion/NameAi.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/fermion/NameAi.java
@@ -4,6 +4,8 @@ import com.thehutgroup.accelerator.connectn.player.Board;
 import com.thehutgroup.accelerator.connectn.player.Counter;
 import com.thehutgroup.accelerator.connectn.player.Player;
 
+import java.util.Random;
+
 
 public class NameAi extends Player {
   public NameAi(Counter counter) {
@@ -17,12 +19,34 @@ public class NameAi extends Player {
     //TODO: make sure said analysis uses less than 2G of heap and returns within 10 seconds on whichever machine is running it
 
 
+    //Things to do:
+    /** Things to do:
+     * Validate moves:
+     *    Track moves made in each column
+     *    Make move within boundary
+     *
+     * Generate move:
+     *    Algorithim move maker
+     *
+     *
+     * Play game:
+     *      Upload dependancie
+     *      Make working local version
+     *
+     * Return a random move that gets validated
+     *
+     *
+     * */
+
+    int randomColumn = (int) (Math.random() * (10));
 
 
 
 
 
+//    return validMove.get(new Random().nextInt(validMove.size())
 
-    return 4;
+    return randomColumn;
   }
 }
+
---
+++ src/main/java/com/thg/accelerator23/connectn/ai/fermion/validMove.java
@@ -0,0 +1,14 @@
+package com.thg.accelerator23.connectn.ai.fermion;
+
+import com.thehutgroup.accelerator.connectn.player.GameConfig;
+
+public class validMove{
+     public validMove(GameConfig config){
+
+     }
+
+
+
+}
+
+
