38c3ea31d6095a8583b0906d691e07bd510da308
Eleanor Kilgallon <eleanor.kilgallon78@thehutgroup.com>
2023-01-09 12:25:44 +0000

    ai knows when column is full

--- src/main/java/com/thg/accelerator23/connectn/ai/rosselanor/ConnectBot.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/rosselanor/ConnectBot.java
@@ -3,6 +3,9 @@ package com.thg.accelerator23.connectn.ai.rosselanor;
 import com.thehutgroup.accelerator.connectn.player.Board;
 import com.thehutgroup.accelerator.connectn.player.Counter;
 import com.thehutgroup.accelerator.connectn.player.Player;
+import com.thehutgroup.accelerator.connectn.player.Position;
+
+import java.util.stream.IntStream;
 
 
 public class ConnectBot extends Player {
@@ -15,6 +18,22 @@ public class ConnectBot extends Player {
   public int makeMove(Board board) {
     //TODO: some crazy analysis
     //TODO: make sure said analysis uses less than 2G of heap and returns within 10 seconds on whichever machine is running it
-    return 4;
+    int position = 4;
+    if (isColumnFull(board, position)){
+      return position + 1;
+    }
+    else {
+      return position;
+    }
   }
+
+  private boolean isColumnFull(Board board, int position) {
+
+    int i = position;
+    return IntStream.range(0, board.getConfig().getHeight())
+            .allMatch(
+                    j -> board.hasCounterAtPosition(new Position(i, j))
+            );
+  }
+
 }
