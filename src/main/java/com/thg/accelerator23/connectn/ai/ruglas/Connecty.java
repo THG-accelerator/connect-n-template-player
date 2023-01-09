ff5a5e55c44483606b3f85c457fd89271057865b
Ruchika Scott <ru.scott24@gmail.com>
2023-01-09 11:49:54 +0000

    Random int generator to get it to work

--- src/main/java/com/thg/accelerator23/connectn/ai/ruglas/Connecty.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/ruglas/Connecty.java
@@ -4,6 +4,8 @@ import com.thehutgroup.accelerator.connectn.player.Board;
 import com.thehutgroup.accelerator.connectn.player.Counter;
 import com.thehutgroup.accelerator.connectn.player.Player;
 
+import java.util.Random;
+
 
 public class Connecty extends Player {
   public Connecty(Counter counter) {
@@ -13,8 +15,8 @@ public class Connecty extends Player {
 
   @Override
   public int makeMove(Board board) {
-    //TODO: some crazy analysis
-    //TODO: make sure said analysis uses less than 2G of heap and returns within 10 seconds on whichever machine is running it
-    return 4;
+    Random rand = new Random();
+    int randomNumber = rand.nextInt(10);
+    return randomNumber;
   }
 }
