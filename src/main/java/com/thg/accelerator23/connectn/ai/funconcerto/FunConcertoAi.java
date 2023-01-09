15ff836d2ef1c6573293ec28f88601877232f002
Joshua Prior <Joshua.Prior03@thehutgroup.com>
2023-01-09 15:31:58 +0000

    Added analyser class and its dependencies

--- src/main/java/com/thg/accelerator23/connectn/ai/funconcerto/FunConcertoAi.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/funconcerto/FunConcertoAi.java
@@ -2,6 +2,7 @@ package com.thg.accelerator23.connectn.ai.funconcerto;
 
 import com.thehutgroup.accelerator.connectn.player.Board;
 import com.thehutgroup.accelerator.connectn.player.Counter;
+import com.thehutgroup.accelerator.connectn.player.GameConfig;
 import com.thehutgroup.accelerator.connectn.player.Player;
 
 
@@ -12,6 +13,9 @@ public class FunConcertoAi extends Player {
 
   @Override
   public int makeMove(Board board) {
+    GameConfig config = new GameConfig(10,8,4);
+    BoardAnalyser analyzer = new BoardAnalyser(config);
+    GameState state = analyzer.calculateGameState(board);
     return 4;
   }
 }
---
+++ src/main/java/com/thg/accelerator23/connectn/ai/funconcerto/Line.java
@@ -0,0 +1,8 @@
+package com.thg.accelerator23.connectn.ai.funconcerto;
+
+import com.thehutgroup.accelerator.connectn.player.Counter;
+
+import java.util.Iterator;
+
+public interface Line extends Iterator<Counter> {
+}
