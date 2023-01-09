4eb69909aa062fa2d5581d4fde218c06376a183d
rosscarmichael31 <Ross.Carmichael31@thehutgroup.com>
2023-01-09 11:17:03 +0000

    initial commit

--- src/main/java/com/thg/accelerator23/connectn/ai/rosselanor/ConnectBot.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/rosselanor/ConnectBot.java
@@ -1,14 +1,14 @@
-package com.thg.accelerator23.connectn.ai.yourteam;
+package com.thg.accelerator23.connectn.ai.rosselanor;
 
 import com.thehutgroup.accelerator.connectn.player.Board;
 import com.thehutgroup.accelerator.connectn.player.Counter;
 import com.thehutgroup.accelerator.connectn.player.Player;
 
 
-public class NameAi extends Player {
-  public NameAi(Counter counter) {
+public class ConnectBot extends Player {
+  public ConnectBot(Counter counter) {
     //TODO: fill in your name here
-    super(counter, NameAi.class.getName());
+    super(counter, ConnectBot.class.getName());
   }
 
   @Override
