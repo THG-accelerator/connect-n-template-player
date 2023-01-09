82612da093efbf8eca318e80963d77a3aeb16a2c
Douglas Haigh <douglashaigh16@thehutgroup.com>
2023-01-09 16:34:02 +0000

    Added some functions to check three in a row and return win position.

--- src/main/java/com/thg/accelerator23/connectn/ai/ruglas/Connecty.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/ruglas/Connecty.java
@@ -1,18 +1,52 @@
 package com.thg.accelerator23.connectn.ai.ruglas;
 
-import com.thehutgroup.accelerator.connectn.player.Board;
-import com.thehutgroup.accelerator.connectn.player.Counter;
-import com.thehutgroup.accelerator.connectn.player.Player;
+import analysis.BoardAnalyser;
+import com.thehutgroup.accelerator.connectn.player.*;
+import model.Line;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 
 public class Connecty extends Player {
+
+  GameConfig config;
+  BoardAnalyser analyser;
+
   public Connecty(Counter counter) {
-    //TODO: fill in your name here
+
     super(counter, Connecty.class.getName());
-    System.out.println("Building");
+
+    GameConfig config = new GameConfig(10,8, 4);
+//    Board board = new Board(config);
+    BoardAnalyser analyser = new BoardAnalyser(config);
   }
+
+  //  Function that returns true if there's three in a row
+  private boolean checkThreeInARow(Board board, Counter counter) {
+
+    ArrayList<Counter> threeInARowArray = new ArrayList<>();
+    int maxInARow = analyser.calculateGameState(board).getMaxInARowByCounter().get(counter);
+
+    return (maxInARow == 3);
+  }
+
+  private boolean canWin(Board board) {
+    return checkThreeInARow(board, this.getCounter()) && isMoveValid( RusFunction(board, this.getCounter()) );
+  }
+  private Position getWinPosition(Board board) {
+    Position[] positions = RusFunction(board);
+    for (Position position : positions) {
+      if (board.isWithinBoard(position) && !board.hasCounterAtPosition(position)) {
+        return position;
+      }
+    }
+    System.out.println("No 3 in a row with an empty space afterwards.");
+    return null;
+  }
+
+
 
   @Override
   public int makeMove(Board board) {
