ace795f97e749895b573087ccac9fe2730beb6e0
Karina Malone <Karina.Malone38@thehutgroup.com>
2023-01-09 13:19:21 +0000

    initial commit

--- src/main/java/com/thg/accelerator23/connectn/ai/politicallyconnect/AlanisMorconnect.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/politicallyconnect/AlanisMorconnect.java
@@ -1,18 +1,21 @@
-package com.thg.accelerator23.connectn.ai.yourteam;
+package com.thg.accelerator23.connectn.ai.politicallyconnect;
 
 import com.thehutgroup.accelerator.connectn.player.Board;
 import com.thehutgroup.accelerator.connectn.player.Counter;
 import com.thehutgroup.accelerator.connectn.player.Player;
 
+import java.util.concurrent.ThreadLocalRandom;
 
-public class NameAi extends Player {
-  public NameAi(Counter counter) {
+
+public class AlanisMorconnect extends Player {
+  public AlanisMorconnect(Counter counter) {
     //TODO: fill in your name here
-    super(counter, NameAi.class.getName());
+    super(counter, AlanisMorconnect.class.getName());
   }
 
   @Override
   public int makeMove(Board board) {
+    //int xMove = ThreadLocalRandom.current().nextInt(0, 9);
     //TODO: some crazy analysis
     //TODO: make sure said analysis uses less than 2G of heap and returns within 10 seconds on whichever machine is running it
     return 4;
