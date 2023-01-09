5e3d8213a2b0cbe8cc5e10fa5f3f41164617fc9d
Eleanor Kilgallon <eleanor.kilgallon78@thehutgroup.com>
2023-01-09 17:01:53 +0000

    made a funtion identifying horizontal trios

--- src/main/java/com/thg/accelerator23/connectn/ai/rosselanor/ConnectBot.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/rosselanor/ConnectBot.java
@@ -1,27 +1,45 @@
 package com.thg.accelerator23.connectn.ai.rosselanor;
 
-import com.thehutgroup.accelerator.connectn.player.Board;
-import com.thehutgroup.accelerator.connectn.player.Counter;
-import com.thehutgroup.accelerator.connectn.player.Player;
-import com.thehutgroup.accelerator.connectn.player.Position;
+import com.thehutgroup.accelerator.connectn.player.*;
+import com.thg.accelerator23.connectn.ai.rosselanor.analysis.BoardAnalyser;
+import com.thg.accelerator23.connectn.ai.rosselanor.analysis.BoardLine;
+import com.thg.accelerator23.connectn.ai.rosselanor.model.Line;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import java.util.concurrent.ThreadLocalRandom;
 
 
 public class ConnectBot extends Player {
+
+  private final String teamCounter;
+
   public ConnectBot(Counter counter) {
     super(counter, ConnectBot.class.getName());
+    String teamCounter = counter.getStringRepresentation();
+    this.teamCounter = teamCounter;
   }
+
+
+
+
 
   @Override
   public int makeMove(Board board) {
   
     int position = ThreadLocalRandom.current().nextInt(0, 10);
-    
+
     if (isColumnFull(board, position)){
-      return position + 1;
+      return position+1;
+    }
+    else if (horizontalTrio(board)){
+      return position;
     }
     else {
       return position;
@@ -37,4 +55,23 @@ public class ConnectBot extends Player {
             );
   }
 
+  private boolean horizontalTrio(Board board) {
+    BoardAnalyser analysis = new BoardAnalyser(board.getConfig());
+    List<Line> lines = analysis.getLines(board);
+    for (int i = 0; i < lines.size(); i++) {
+      Map result = analysis.getBestRunByColour(lines.get(i));
+      if (result.containsValue(3)) {
+        System.out.println(result);
+        int comparison = (int) result.get(Counter.X);
+        if (comparison == 3) {
+          System.out.println('X');
+        }
+        else {
+          System.out.println('O');
+        }
+        return true;
+      }
+    }
+    return false;
+  }
 }
