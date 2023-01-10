e5ad5c0b29c44fb19c0e6b7b03b1e23885289466
Karina Malone <Karina.Malone38@thehutgroup.com>
2023-01-10 10:57:41 +0000

    basic logic for random ai

--- src/main/java/com/thg/accelerator23/connectn/ai/politicallyconnect/AlanisFourconnect.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/politicallyconnect/AlanisFourconnect.java
@@ -1,23 +1,29 @@
 package com.thg.accelerator23.connectn.ai.politicallyconnect;
 
-import com.thehutgroup.accelerator.connectn.player.Board;
-import com.thehutgroup.accelerator.connectn.player.Counter;
-import com.thehutgroup.accelerator.connectn.player.Player;
+import com.thehutgroup.accelerator.connectn.player.*;
 
 import java.util.concurrent.ThreadLocalRandom;
 
 
-public class AlanisMorconnect extends Player {
-  public AlanisMorconnect(Counter counter) {
+public class AlanisFourconnect extends Player {
+  public AlanisFourconnect(Counter counter) {
     //TODO: fill in your name here
-    super(counter, AlanisMorconnect.class.getName());
+    super(counter, AlanisFourconnect.class.getName());
   }
 
   @Override
   public int makeMove(Board board) {
-    int xMove = ThreadLocalRandom.current().nextInt(0, 9);
+    Position position = new Position(0,5);
+    System.out.printf(String.valueOf(board.hasCounterAtPosition(position)));
+//    int middleColumn = (int) Math.floor((board.getConfig().getWidth() / 2));
+//    for (int i = 0; i < board.getConfig().getWidth(); i++) {
+    if (!board.hasCounterAtPosition(position)){
+      return (int) Math.floor((board.getConfig().getWidth() / 2));
+    } else {
+      System.out.println("lol");
+      return ThreadLocalRandom.current().nextInt(0, 9);
+    }
     //TODO: some crazy analysis
     //TODO: make sure said analysis uses less than 2G of heap and returns within 10 seconds on whichever machine is running it
-    return xMove;
   }
 }
