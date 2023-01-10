8c54b4b1a941ff82739f875fd3ef1edee43a312a
rosscarmichael31 <Ross.Carmichael31@thehutgroup.com>
2023-01-10 10:09:50 +0000

    minimax runs

--- src/main/java/com/thg/accelerator23/connectn/ai/rosselanor/ConnectBot.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/rosselanor/ConnectBot.java
@@ -2,25 +2,34 @@ package com.thg.accelerator23.connectn.ai.rosselanor;
 
 import com.thehutgroup.accelerator.connectn.player.Board;
 import com.thehutgroup.accelerator.connectn.player.Counter;
+import com.thehutgroup.accelerator.connectn.player.InvalidMoveException;
 import com.thehutgroup.accelerator.connectn.player.Player;
 import com.thg.accelerator23.connectn.ai.rosselanor.analysis.BoardAnalyser;
 
 
 public class ConnectBot extends Player {
     Counter myCounter;
-    BoardAnalyser boardAnalyser;
 
-    public ConnectBot(Counter counter) {
-        super(counter, ConnectBot.class.getName());
-        this.myCounter = counter;
+    BoardAnalyser boardAnalyser;
+    MiniMaxAI miniMaxAI;
+
+
+    public ConnectBot(Counter myCounter) {
+        super(myCounter, ConnectBot.class.getName());
+        this.myCounter = myCounter;
+        miniMaxAI = new MiniMaxAI(5, myCounter);
     }
 
     @Override
     public int makeMove(Board board) {
+        miniMaxAI.setBoard(board);
+        miniMaxAI.setBoardAnalyser(new BoardAnalyser(board.getConfig()));
 
-        MiniMaxAI miniMaxAI = new MiniMaxAI(5, myCounter, board);
-
-      return miniMaxAI.runMiniMax();
-    };
+        try {
+            return miniMaxAI.getMove();
+        } catch (InvalidMoveException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 }
