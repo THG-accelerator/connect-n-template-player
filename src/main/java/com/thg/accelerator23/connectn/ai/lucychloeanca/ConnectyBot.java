bf5ae171a9a697b57bb70a005bf644dbfb108703
Lucy Handford <lucy.handford73@thehutgroup.com>
2023-01-10 16:24:54 +0000

    Added minimiser function and copied over board analyser.

--- src/main/java/com/thg/accelerator23/connectn/ai/lucychloeanca/ConnectyBot.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/lucychloeanca/ConnectyBot.java
@@ -1,11 +1,9 @@
 package com.thg.accelerator23.connectn.ai.lucychloeanca;
 
-import com.thehutgroup.accelerator.connectn.player.Board;
-import com.thehutgroup.accelerator.connectn.player.Counter;
-import com.thehutgroup.accelerator.connectn.player.Player;
-import com.thehutgroup.accelerator.connectn.player.Position;
+import com.thehutgroup.accelerator.connectn.player.*;
 
 import java.util.Random;
+import java.util.stream.IntStream;
 
 
 public class ConnectyBot extends Player {
@@ -38,6 +36,28 @@ public class ConnectyBot extends Player {
       }
     }
     return false;
+  }
+
+
+  private int miniMax (int position, int depth, boolean maximisingPlayer, Board board){
+    if (depth == 0 || new LCABoardAnalyser(board.getConfig()).calculateGameState(board).isEnd()) {
+      return columnScore();
+    }
+    if (maximisingPlayer){
+      int maxEval = -1000000;
+      for (int i = 0; i < board.getConfig().getWidth(); i++){
+        int eval = miniMax(i , depth - 1, false, board);
+        maxEval = Math.max(maxEval, eval);
+      }
+      return maxEval;
+    } else {
+      int minEval = 1000000;
+      for (int i = 0; i < board.getConfig().getWidth(); i++){
+        int eval = miniMax(i, depth -1, true, board);
+        minEval = Math.min(minEval, eval);
+      }
+      return minEval;
+    }
   }
 
 }
