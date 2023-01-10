b67e9ca24e0af8ee000c164570843971083a6fc7
Bethan Cropper <Bethan.Cropper03@thehutgroup.com>
2023-01-10 16:55:11 +0000

    made tree class, not yet defined getLeaves and getChildren, which should be useful for the minimax

---
+++ src/main/java/com/thg/accelerator23/connectn/ai/funconcerto/movetree/EmptyTree.java
@@ -0,0 +1,2 @@
+package com.thg.accelerator23.connectn.ai.funconcerto.movetree;public class EmptyTree {
+}
---
+++ src/main/java/com/thg/accelerator23/connectn/ai/funconcerto/movetree/MoveTree.java
@@ -0,0 +1,55 @@
+package com.thg.accelerator23.connectn.ai.funconcerto.movetree;
+
+import java.util.ArrayList;
+import java.util.List;
+import com.thehutgroup.accelerator.connectn.player.Board;
+import com.thehutgroup.accelerator.connectn.player.Counter;
+import com.thehutgroup.accelerator.connectn.player.GameConfig;
+
+public class MoveTree {
+    private final Counter counter;
+    private final int depth;
+    private final Board state;
+    private int mostRecentPosition;
+    private List<MoveTree> moveList;
+
+    public MoveTree() {
+        this.counter = Counter.O;
+        this.depth = 1;
+        GameConfig config = new GameConfig(10, 8, 4);
+        this.state = new Board(config);
+    }
+
+    public MoveTree(Counter counter, int depth, Board state, int position){
+        this.counter = counter;
+        this.depth = depth;
+        this.state = state;
+        this.mostRecentPosition = position;
+    }
+
+
+    public Counter getCounter(){
+        return this.counter;
+    }
+    public int getDepth(){
+        return this.depth;
+    }
+
+    public Board getState(){
+        return this.state;
+    }
+
+    public int getPosition(){
+        return this.mostRecentPosition;
+    }
+
+    public void addNode(int position){
+        Counter newCounter = counter.getOther();
+        int newDepth = depth + 1;
+        Board newState = new Board(state, position, newCounter);
+        MoveTree newNode = new MoveTree(counter.getOther(), depth + 1, newState, position);
+    }
+
+    public List<MoveTree> getLeaves(){}
+    public List<MoveTree> getChildren(){}
+}
