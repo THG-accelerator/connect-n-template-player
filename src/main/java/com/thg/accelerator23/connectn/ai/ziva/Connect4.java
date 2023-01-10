08862a07f221b1e72b2e5752623737f4c4b3796e
Ziva Sacharissa <ziva.sacharissa84@thehutgroup.com>
2023-01-10 10:43:54 +0000

    first commit

--- src/main/java/com/thg/accelerator23/connectn/ai/ziva/Connect4.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/ziva/Connect4.java
@@ -1,14 +1,14 @@
-package com.thg.accelerator23.connectn.ai.yourteam;
+package com.thg.accelerator23.connectn.ai.ziva;
 
 import com.thehutgroup.accelerator.connectn.player.Board;
 import com.thehutgroup.accelerator.connectn.player.Counter;
 import com.thehutgroup.accelerator.connectn.player.Player;
 
 
-public class NameAi extends Player {
-  public NameAi(Counter counter) {
+public class Connect4 extends Player {
+  public Connect4(Counter counter) {
     //TODO: fill in your name here
-    super(counter, NameAi.class.getName());
+    super(counter, Connect4.class.getName());
   }
 
   @Override
