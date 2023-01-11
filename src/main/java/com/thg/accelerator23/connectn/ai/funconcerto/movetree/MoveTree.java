453b72b428f369bd3b74600fb55245dd2fadd8dc
Bethan Cropper <Bethan.Cropper03@thehutgroup.com>
2023-01-11 10:27:07 +0000

    made empty functions return something so the code doesn't break

--- src/main/java/com/thg/accelerator23/connectn/ai/funconcerto/movetree/MoveTree.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/funconcerto/movetree/MoveTree.java
@@ -10,7 +10,7 @@ public class MoveTree {
     private final Counter counter;
     private final int depth;
     private final Board state;
-    private int mostRecentPosition;
+    private int fromWhichInitialMove;
     private List<MoveTree> moveList;
 
     public MoveTree() {
@@ -49,7 +49,15 @@ public class MoveTree {
         Board newState = new Board(state, position, newCounter);
         MoveTree newNode = new MoveTree(counter.getOther(), depth + 1, newState, position);
     }
+    public void removeNode(List<Integer> moveListToGetToNode){
+        System.out.println('sjhviw');
+    }
+    public List<MoveTree> getLeaves(){
+        List<MoveTree> leafList = new ArrayList<>();
+        return leafList;
+    }
+    public List<MoveTree> getChildren(){
+        return moveList;
+    }
 
-    public List<MoveTree> getLeaves(){}
-    public List<MoveTree> getChildren(){}
 }
