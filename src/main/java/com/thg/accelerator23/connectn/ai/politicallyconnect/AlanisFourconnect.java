c27b595ad2609b997ea662d29950653c8aa24037
Ciara Shields <ciara.shelds80@thehutgroup.com>
2023-01-11 11:26:00 +0000

    copied analysis package

--- src/main/java/com/thg/accelerator23/connectn/ai/politicallyconnect/AlanisFourconnect.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/politicallyconnect/AlanisFourconnect.java
@@ -1,6 +1,7 @@
 package com.thg.accelerator23.connectn.ai.politicallyconnect;
 
 import com.thehutgroup.accelerator.connectn.player.*;
+import com.thg.accelerator23.connectn.ai.politicallyconnect.analyser.BoardAnalyser;
 
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -17,15 +18,22 @@ public class AlanisFourconnect extends Player {
 
 //    int middleColumn = (int) Math.floor((board.getConfig().getWidth() / 2));
 //    for (int i = 0; i < board.getConfig().getWidth(); i++) {
-    if (!board.hasCounterAtPosition(new Position(5, 0))){
+    if (!board.hasCounterAtPosition(new Position(5, 0))) {
 //      System.out.println(board.getCounterAtPosition(new Position(5, 0)));
 //      System.out.println(!board.hasCounterAtPosition(new Position(0, 5)));
       return (int) Math.floor((board.getConfig().getWidth() / 2));
     } else {
-      System.out.println("lol");
-      return ThreadLocalRandom.current().nextInt(0, 9);
+      return winOnNext(board);
+//      System.out.println("lol");
+//      return ThreadLocalRandom.current().nextInt(0, 9);
     }
-    //TODO: some crazy analysis
-    //TODO: make sure said analysis uses less than 2G of heap and returns within 10 seconds on whichever machine is running it
+
   }
-}
+
+    public int winOnNext(Board board) {
+      if ()
+      return ThreadLocalRandom.current().nextInt(0, 9);
+      //TODO: some crazy analysis
+      //TODO: make sure said analysis uses less than 2G of heap and returns within 10 seconds on whichever machine is running it
+    }
+  }
