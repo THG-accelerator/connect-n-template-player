7fb08658806fa6ae93fd24703357f6dc3c371f1b
Ruchika Scott <ru.scott24@gmail.com>
2023-01-11 13:49:04 +0000

    Scoring

---
+++ src/main/java/com/thg/accelerator23/connectn/ai/ruglas/GetScore.java
@@ -0,0 +1,103 @@
+package com.thg.accelerator23.connectn.ai.ruglas;
+
+import com.thehutgroup.accelerator.connectn.player.Board;
+import com.thehutgroup.accelerator.connectn.player.Counter;
+import com.thehutgroup.accelerator.connectn.player.InvalidMoveException;
+import com.thehutgroup.accelerator.connectn.player.Position;
+import com.thg.accelerator23.connectn.ai.ruglas.analysis.BoardAnalyser;
+import com.thg.accelerator23.connectn.ai.ruglas.analysis.GameState;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GetScore {
+    Board board;
+    BoardAnalyser boardAnalyser;
+
+    public GetScore(Board board) {
+        this.board = board;
+        this.boardAnalyser = new BoardAnalyser(this.board.getConfig());
+    }
+
+    public int getTotalScore(Position positionToCheck, Board boardToCheck, Counter counter) throws InvalidMoveException {
+        GameState gameState = boardAnalyser.calculateGameState(boardToCheck);
+        if(gameState.isDraw()){return 0;} else if (gameState.isWin()) { return 10000;
+
+        }
+        else{
+            return getScoreFromAgjPositions(positionToCheck, boardToCheck, counter);
+        }
+
+
+    }
+    public ArrayList<ArrayList<Position>> getAdjacentNPositions(Position position, int n) {
+        ArrayList<ArrayList<Position>> positions = new ArrayList<>();
+
+        ArrayList<Position> horizontal = new ArrayList<>();
+        ArrayList<Position> vertical = new ArrayList<>();
+        ArrayList<Position> upwardsDiagonal = new ArrayList<>();
+        ArrayList<Position> downwardsDiagonal = new ArrayList<>();
+
+        int x = position.getX();
+        int y = position.getY();
+
+        for ( int i=1-n; i < n ; i++) {
+
+            Position horizontalPosition = new Position(x+i, y);
+            Position verticalPosition = new Position(x,y+i);
+            Position upwardsDiagonalPosition = new Position(x+i,y+i);
+            Position downwardsDiagonalPosition = new Position(x+i,y-i);
+
+            if (board.isWithinBoard(horizontalPosition)) {
+                horizontal.add(horizontalPosition);
+            }
+            if (board.isWithinBoard(verticalPosition)) {
+                vertical.add(verticalPosition);
+            }
+            if (board.isWithinBoard(upwardsDiagonalPosition)) {
+                upwardsDiagonal.add(upwardsDiagonalPosition);
+            }
+
+            if (board.isWithinBoard(downwardsDiagonalPosition)) {
+                downwardsDiagonal.add(downwardsDiagonalPosition);
+            }
+
+        }
+        positions.add(horizontal);
+        positions.add(vertical);
+        positions.add(upwardsDiagonal);
+        positions.add(downwardsDiagonal);
+
+        return positions;
+    }
+
+    public int getScoreFromAgjPositions(Position positionToPlay, Board board, Counter counter) throws InvalidMoveException {
+        ArrayList<ArrayList<Position>> positionsArray = getAdjacentNPositions(positionToPlay, 4);
+        int score = 0;
+        for (ArrayList<Position> positions : positionsArray) {
+            for (int positionIndex = 0; positionIndex<positions.size()-4; positionIndex++) {
+                List<Counter> counterList = new ArrayList<>();
+                if(board.getCounterAtPosition(positions.get(positionIndex)) == counter.getOther()){}
+                else {
+                    for (int counterIndex = 0; counterIndex < 4; counterIndex++) {
+                        counterList.add(board.getCounterAtPosition(positions.get(positionIndex + counterIndex)));
+                    }
+                    score += findScore(counterList, counter);
+                }
+
+            }
+        }
+        return score;
+    }
+
+    private int findScore(List<Counter> counterList, Counter playerCounter) {
+        if(counterList.stream().filter(counter -> counter== playerCounter.getOther()).count() > 0) {return 0;}
+        else{
+            long counterCount = counterList.stream().filter(counter -> counter== playerCounter).count();
+            if(counterCount == 2){return 10;}
+            else if(counterCount == 3) {return 20;}
+            else return 0;
+        }
+
+    }
+}
