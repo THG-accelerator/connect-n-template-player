51564a7cea3a7e931101f4457d722110c1ba8306
Chloe Brown <chloe.brown65@thehutgroup.com>
2023-01-12 15:12:55 +0000

    implemented minimax

---
+++ src/main/java/com/thg/accelerator23/connectn/ai/lucychloeanca/ColumnScore.java
@@ -0,0 +1,27 @@
+package com.thg.accelerator23.connectn.ai.lucychloeanca;
+
+public class ColumnScore {
+    int Column;
+    int Score;
+
+    public ColumnScore(Integer column, int score) {
+        Column = column;
+        Score = score;
+    }
+
+    public int getColumn() {
+        return Column;
+    }
+
+    public void setColumn(int column) {
+        Column = column;
+    }
+
+    public int getScore() {
+        return Score;
+    }
+
+    public void setScore(int score) {
+        Score = score;
+    }
+}
--- src/main/java/com/thg/accelerator23/connectn/ai/lucychloeanca/ConnectyBot.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/lucychloeanca/ConnectyBot.java
@@ -3,7 +3,6 @@ package com.thg.accelerator23.connectn.ai.lucychloeanca;
 import com.thehutgroup.accelerator.connectn.player.*;
 
 import java.util.*;
-import java.util.stream.IntStream;
 
 
 public class ConnectyBot extends Player {
@@ -14,7 +13,13 @@ public class ConnectyBot extends Player {
 
   @Override
   public int makeMove(Board board) {
-    int position = getOptimalCol(board);
+    int position;
+    try {
+      position = miniMax(3, true, board).getColumn();
+      //position = getOptimalCol(board, this.getCounter());
+    } catch (InvalidMoveException e) {
+      throw new RuntimeException(e);
+    }
     //TODO: some crazy analysis
     //TODO: make sure said analysis uses less than 2G of heap and returns within 10 seconds on whichever machine is running it
     return position;
@@ -48,71 +53,203 @@ public class ConnectyBot extends Player {
     return availableCol;
   }
 
+  private Counter getOpponentCounter(Counter counter){
 
-  private int columnScoreCalculator(Board board, int columnNumber){
+    Counter opponentCounter = null;
+    switch (counter) {
+      case X -> opponentCounter = Counter.O;
+      case O -> opponentCounter = Counter.X;
+    }
+    return opponentCounter;
+  }
+
+  private int evaluateWindow(List<Counter> window, Counter counter){
+
     int score = 0;
-    try {
-      Board possibleNewBoard = new Board(board, columnNumber, getCounter());
-      GameConfig newConfig = possibleNewBoard.getConfig();
-      LCABoardAnalyser boardAnalyser = new LCABoardAnalyser(newConfig);
-      if (boardAnalyser.calculateGameState(possibleNewBoard).isEnd()) {
-        Counter winner = boardAnalyser.calculateGameState(possibleNewBoard).getWinner();
-        if (winner.equals(this.getCounter())) {
-          score = 1;
-        }else {
-          score = -1;
-        }
-      } else {
-        score = 0;
-      }
+    Counter opponentCounter = getOpponentCounter(counter);
+    int count = Collections.frequency(window, counter);
+    int empty = Collections.frequency(window, null);
+    int opponentCount = Collections.frequency(window, opponentCounter);
+    if (count == 4){
+      score = score + 1000000;
+    } else if (count == 3 && empty == 1) {
+      score = score +1000;
+    }
 
-    }catch (InvalidMoveException e){
-      score = -100000000;
+    if (opponentCount == 3 && empty==1){
+      score = score - 8000;
+    }
+    return score;
+  }
+
+  private int scoreCenterArray(Board board, Counter counter){
+
+    List<Integer> availableCol = getAvailableCol(board);
+    int score =0;
+    int middleCol = Math.floorDiv(board.getConfig().getWidth(), 2);
+    if (availableCol.contains(middleCol)) {
+      List<Counter> centerList = new ArrayList<>();
+      for (int row = 0; row < board.getConfig().getHeight(); row++) {
+        Counter currentCounter = board.getCounterAtPosition(new Position(middleCol, row));
+        centerList.add(currentCounter);
+      }
+      int centerCount = Collections.frequency(centerList, counter);
+      score = score + (centerCount * 100);
+    }
+    return score;
+
+  }
+
+  private int horizontalScoreCalculator(Board board, Counter counter){
+
+    int score =0;
+    for (int row = 0; row<board.getConfig().getHeight(); row++){
+      List<Counter> rowList = new ArrayList<>();
+      for(int col=0; col<board.getConfig().getWidth(); col++){
+        Counter countInPosition = board.getCounterAtPosition(new Position(col,row));
+        rowList.add(countInPosition);
+      }
+      for (int col=0; col<rowList.size()-3; col++){
+        List<Counter> window = rowList.subList(col, col+4);
+        score = score + evaluateWindow(window, counter);
+
+      }
+    }
+    return score;
+  }
+
+  private int verticalScoreCalculator(Board board, Counter counter){
+
+    int score =0;
+    for (int col=0; col<board.getConfig().getWidth(); col++){
+      List<Counter> colList = new ArrayList<>();
+      for (int row = 0; row<board.getConfig().getHeight(); row++){
+        Counter countInPosition = board.getCounterAtPosition(new Position(col,row));
+        colList.add(countInPosition);
+      }
+      for (int row = 0; row< colList.size()-3; row++){
+        List<Counter> window = colList.subList(row, row+4);
+        score = score + evaluateWindow(window, counter);
+      }
     }
 
     return score;
   }
 
-  private int miniMax (int position, int depth, boolean maximisingPlayer, Board board){
-    if (depth == 0 || new LCABoardAnalyser(board.getConfig()).calculateGameState(board).isEnd()) {
-      return columnScoreCalculator(board, position);
-    }
-    if (maximisingPlayer){
-      int maxEval = -1000000;
-      for (int i = 0; i < board.getConfig().getWidth(); i++){
-        int eval = miniMax(i , depth - 1, false, board);
-        maxEval = Math.max(maxEval, eval);
+  private int positiveDiagonalScoreCalculator(Board board, Counter counter){
+
+    int score =0;
+    for (int row=0; row<board.getConfig().getHeight()-3;row++){
+
+      for (int col =0; col<board.getConfig().getWidth()-3;col++){
+        List<Counter> window = new ArrayList<>();
+        for (int length = 0; length<4; length++){
+          Counter currentPlace = board.getCounterAtPosition(new Position(col+length,row+length));
+          window.add(currentPlace);
+        }
+        score = score + evaluateWindow(window, counter);
       }
-      return maxEval;
-    } else {
-      int minEval = 1000000;
-      for (int i = 0; i < board.getConfig().getWidth(); i++){
-        int eval = miniMax(i, depth -1, true, board);
-        minEval = Math.min(minEval, eval);
-      }
-      return minEval;
     }
+
+    return score;
   }
 
-  private Map<Integer, Integer> getAllColScores(Board board){
-    Map<Integer, Integer> colScores = new HashMap<>();
+  private int negativeDiagonalScoreCalculator(Board board, Counter counter){
+
+    int score =0;
+    for (int row=0; row<board.getConfig().getHeight()-3;row++){
+      for (int col =0; col<board.getConfig().getWidth()-3;col++){
+        List<Counter> window = new ArrayList<>();
+        for (int length = 0; length<4; length++){
+          Counter currentPlace = board.getCounterAtPosition(new Position(col+length,row+3-length));
+          window.add(currentPlace);
+        }
+        score = score + evaluateWindow(window, counter);
+      }
+    }
+
+    return score;
+  }
+
+
+
+  private int scoreCalculator(Board board, Counter counter){
+    return (horizontalScoreCalculator(board, counter)
+            +verticalScoreCalculator(board, counter)
+            +positiveDiagonalScoreCalculator(board, counter)
+            +negativeDiagonalScoreCalculator(board, counter)
+            +scoreCenterArray(board,counter));
+  }
+
+  private ColumnScore miniMax (int depth, boolean maximisingPlayer, Board board) throws InvalidMoveException {
+    Counter counter = this.getCounter();
+    if (!maximisingPlayer) {
+      counter = getOpponentCounter(counter);
+    }
+    LCABoardAnalyser boardAnalyser = new LCABoardAnalyser(board.getConfig());
+    if (depth == 0 || boardAnalyser.calculateGameState(board).isEnd()) {
+      if (depth == 0) {
+        int finalScore = scoreCalculator(board, counter);
+        return new ColumnScore(randomMove(board) ,finalScore);
+      }
+      else {
+        if (boardAnalyser.calculateGameState(board).getWinner().equals(this.getCounter())) {
+          return new ColumnScore(randomMove(board) ,1000000000);
+        } else if (boardAnalyser.calculateGameState(board).getWinner().equals(getOpponentCounter(this.getCounter()))) {
+          return new ColumnScore(randomMove(board) ,-1000000000);
+        } else {
+          return new ColumnScore(randomMove(board) ,0);
+        }
+      }
+
+    }
     List<Integer> availableCol = getAvailableCol(board);
-    availableCol.stream().forEach((colNumber) -> {
-      int score = miniMax(colNumber, 3, true, board);
-      colScores.put(colNumber, score);
-    });
-    return colScores;
-  }
+    if (maximisingPlayer){
+      int maxEval = -1000000000;
+      int bestCol = randomMove(board);
+      for (int col: availableCol){
+        Board newPossibleBoard = new Board(board, col, this.getCounter());
+        int eval = miniMax( depth - 1, false, newPossibleBoard).getScore();
+        maxEval = Math.max(maxEval, eval);
+        if (eval> maxEval){
+          maxEval = eval;
+          bestCol = col;
 
-  private int getOptimalCol(Board board){
-    Map<Integer, Integer> colScores = getAllColScores(board);
-    int highestScoringCol = colScores.entrySet().stream().max((entry1, entry2) -> entry1.getValue() > entry2.getValue() ? 1 : -1).get().getKey();
-    int highestScore = colScores.get(highestScoringCol);
-    if (highestScore>0) {
-      return highestScoringCol;
-    }else {
-      System.out.println("random");
-      return randomMove(board);
+        }
+      }
+      return new ColumnScore(bestCol, maxEval);
+    } else {
+      int minEval = 1000000000;
+      int bestCol = randomMove(board);
+      for (int col: availableCol){
+        Board newPossibleBoard = new Board(board, col, getOpponentCounter(this.getCounter()));
+        int eval = miniMax( depth -1, true, newPossibleBoard).getScore();
+        if (eval<minEval){
+          minEval = eval;
+          bestCol = col;
+        }
+      }
+      return new ColumnScore(bestCol, minEval);
     }
   }
+
+  private int getOptimalCol(Board board, Counter counter) throws InvalidMoveException {
+    int bestScore = 0;
+    int bestCol = randomMove(board);
+    List<Integer> availableCol = getAvailableCol(board);
+    for (int col =0; col<availableCol.size(); col++) {
+      Board newPossibleBoard = new Board(board, col, counter);
+      int score = scoreCalculator(newPossibleBoard, counter);
+      if (score>bestScore){
+        bestScore = score;
+        bestCol = col;
+      }
+
+    }
+    return bestCol;}
+
+
 }
+
+
+
