e550167e059feb9e8677885bea52b1b57202631c
Naoki Ueda <naoki.ueda05@thehutgroup.com>
2023-01-12 15:58:13 +0000

    minimax depth 1

--- src/main/java/com/thg/accelerator23/connectn/ai/ultimatewinner/UltimateWinner.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/ultimatewinner/UltimateWinner.java
@@ -4,7 +4,9 @@ import com.thehutgroup.accelerator.connectn.player.*;
 
 import static java.lang.Math.random;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 import static java.lang.Math.random;
 import static java.util.Collections.list;
@@ -12,7 +14,6 @@ import static java.util.Collections.list;
 
 
 public class UltimateWinner extends Player {
-  public int depth = 4;
 
   public UltimateWinner(Counter counter) {
     //TODO: fill in your name here
@@ -29,18 +30,87 @@ public class UltimateWinner extends Player {
   boolean maximizingPlayer = true;
   @Override
   public int makeMove(Board board) {
-    int chosenMove = minimaxChoice(board, 4, maximizingPlayer);
-    int randomNum = (int)(random() * 9);
-    minimaxChoice(board, 4, maximizingPlayer);
-    System.out.println(randomNum);
-    return randomNum;
+//    int chosenMove = minimaxChoice(board, 4, maximizingPlayer);
+//    int randomNum = (int)(random() * 9);
+//    minimaxChoice(board, 4, maximizingPlayer);
+//    Counter[] counters = new Counter[4];
+//    counters[0] = this.getCounter();
+//    counters[1] = this.getCounter();
+//    counters[2] = this.getCounter();
+//    counters[3] = this.getCounter();
+//    int test = evaluateWindow(counters, this.getCounter());
+//    System.out.println(counters[0]);
+//    System.out.println(counters[1]);
+//    System.out.println(counters[2]);
+//    System.out.println(counters[3]);
+//    System.out.println(test);
+//    int test2 = scorePosition(board, this.getCounter());
+//    System.out.println(test2);
+    int column = minimax(board, 1, true).getColumn();
+
+    return column;
+  }
+  public MinimaxReturn minimax(Board board, int depth, boolean maximizingPlayer) {
+    BoardAnalyser boardAnalyser = new BoardAnalyser(board.getConfig());
+    GameState gameState = boardAnalyser.calculateGameState(board);
+    if (depth == 0 || gameState.isEnd()) {
+      if (gameState.isEnd()){
+        if (winningMove(board, this.getCounter())) {
+          return new MinimaxReturn( -1,1000000000);
+        } else if (winningMove(board, this.getCounter().getOther())) {
+          return new MinimaxReturn( -1,-1000000000);
+        } else {
+          return new MinimaxReturn(-1, 0);
+        }
+      } else {
+        return new MinimaxReturn(-1, scorePosition(board, this.getCounter()));
+      }
+    }
+    if (maximizingPlayer) {
+      int value = -1000000000;
+      int column = 0;
+      for (int i=0; i < board.getConfig().getWidth(); i++) {
+        if (!board.hasCounterAtPosition(new Position(i, board.getConfig().getHeight()-1))){
+          try {
+            Board boardCopy = new Board(board, i, this.getCounter());
+            int newScore = minimax(boardCopy, depth-1, true).getValue();
+
+            if (newScore > value) {
+              value = newScore;
+              column = i;
+            }
+          } catch (InvalidMoveException e) {
+            System.out.println("Error occurred");
+          }
+        };
+      } return new MinimaxReturn(column, value);
+    } else {
+      int value = 1000000000;
+      int column = 0;
+      for (int i=0; i < board.getConfig().getWidth(); i++) {
+        if (!board.hasCounterAtPosition(new Position(i, board.getConfig().getHeight()-1))){
+          try {
+            Board boardCopy = new Board(board, i, this.getCounter().getOther());
+            int newScore = minimax(boardCopy, depth - 1, false).getValue();
+
+            if (newScore < value) {
+              value = newScore;
+              column = i;
+            }
+          } catch (InvalidMoveException e) {
+            System.out.println("Error occurred");
+          }
+        };
+      } return new MinimaxReturn(column, value);
+    }
+
   }
 
   public boolean winningMove(Board board, Counter counter) {
     for (int columns = 0; columns < board.getConfig().getWidth(); columns++) {
       for (int rows = 0; rows < board.getConfig().getHeight() -3; rows++) {
         if (board.getCounterAtPosition(new Position(columns, rows)) == counter && board.getCounterAtPosition(new Position(columns, rows + 1)) == counter && board.getCounterAtPosition(new Position(columns, rows + 2)) == counter && board.getCounterAtPosition(new Position(columns, rows + 3)) == counter) {
-          System.out.println("4 in row vertical");
+
           return true;
         }
       }
@@ -48,7 +118,7 @@ public class UltimateWinner extends Player {
     for (int columns = 0; columns < board.getConfig().getWidth() -3; columns++) {
       for (int rows = 0; rows < board.getConfig().getHeight(); rows++) {
         if (board.getCounterAtPosition(new Position(columns, rows)) == counter && board.getCounterAtPosition(new Position(columns + 1, rows)) == counter && board.getCounterAtPosition(new Position(columns + 2, rows)) == counter && board.getCounterAtPosition(new Position(columns + 3, rows)) == counter) {
-          System.out.println("4 in row horizontal");
+
           return true;
         }
       }
@@ -56,7 +126,7 @@ public class UltimateWinner extends Player {
     for (int columns = 0; columns < board.getConfig().getWidth() -3; columns++) {
       for (int rows = 0; rows < board.getConfig().getHeight() -3; rows++) {
         if (board.getCounterAtPosition(new Position(columns, rows)) == counter && board.getCounterAtPosition(new Position(columns +1, rows +1)) == counter && board.getCounterAtPosition(new Position(columns +2, rows +2)) == counter && board.getCounterAtPosition(new Position(columns +3, rows +3)) == counter) {
-          System.out.println("4 in row diagonal");
+
           return true;
         }
       }
@@ -64,7 +134,7 @@ public class UltimateWinner extends Player {
     for (int columns = 0; columns < board.getConfig().getWidth() -3; columns++) {
       for (int rows = 3; rows < board.getConfig().getHeight(); rows++) {
         if (board.getCounterAtPosition(new Position(columns, rows)) == counter && board.getCounterAtPosition(new Position(columns +1, rows -1)) == counter && board.getCounterAtPosition(new Position(columns +2, rows -2)) == counter && board.getCounterAtPosition(new Position(columns +3, rows -3)) == counter) {
-          System.out.println("4 in row diagonal");
+
           return true;
         }
       }
@@ -87,22 +157,26 @@ public class UltimateWinner extends Player {
     int otherCount = 0;
     int emptyCount = 0;
     for (Counter current : window){
-      if (current.getStringRepresentation() == counter.getStringRepresentation()){
-        playerCount++;
-      } else if (current.getStringRepresentation() == other.getStringRepresentation()) {
-        otherCount++;
+      if (current != null) {
+        if (current.getOther().getOther() == counter.getOther().getOther()) {
+          playerCount++;
+        } else if (current.getOther().getOther() == other) {
+          otherCount++;
+        }
       } else {
         emptyCount++;
       }
     }
     if (playerCount == 4) {
-      score += 100;
+      score += 1500;
     } else if (playerCount == 3 && emptyCount == 1) {
-      score += 5;
+      score += 300;
     } else if (playerCount == 2 && emptyCount == 2) {
-      score += 2;
+      score += 100;
     }if (otherCount == 3 && emptyCount == 1) {
-      score -= 4;
+      score -= 1000;
+    } else if (otherCount == 2 && emptyCount == 2) {
+      score -= 500;
     }
     return score;
   }
@@ -110,6 +184,14 @@ public class UltimateWinner extends Player {
   //minute 15
   public int scorePosition (Board board, Counter counter) {
     int score = 0;
+    int centerPosition = board.getConfig().getWidth()/2 - 1;
+    ArrayList<Counter> centerArray = new ArrayList<>();
+    for (int n=0; n < board.getConfig().getHeight(); n++) {
+      centerArray.add(board.getCounterAtPosition(new Position(centerPosition, n)));
+    }
+    int centerCount = Collections.frequency(centerArray, counter);
+
+    score += centerCount * 3;
 
     // Horizontal
     for (int r = 0; r < board.getConfig().getHeight(); r++) {
