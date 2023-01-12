a2404e9d173d026d17daf28b1b22ef1ffd9e35f4
InesCortina <Ines.Cortina91@thehutgroup.com>
2023-01-12 09:50:28 +0000

    score and winning function merged

--- src/main/java/com/thg/accelerator23/connectn/ai/ultimatewinner/UltimateWinner.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/ultimatewinner/UltimateWinner.java
@@ -3,6 +3,12 @@ package com.thg.accelerator23.connectn.ai.ultimatewinner;
 import com.thehutgroup.accelerator.connectn.player.*;
 
 import static java.lang.Math.random;
+import java.util.ArrayList;
+import java.util.Random;
+
+import static java.lang.Math.random;
+import static java.util.Collections.list;
+//import static sun.nio.ch.DatagramChannelImpl.AbstractSelectableChannels.forEach;
 
 
 public class UltimateWinner extends Player {
@@ -29,6 +35,7 @@ public class UltimateWinner extends Player {
     System.out.println(randomNum);
     return randomNum;
   }
+
   public boolean winningMove(Board board, Counter counter) {
     for (int columns = 0; columns < board.getConfig().getWidth(); columns++) {
       for (int rows = 0; rows < board.getConfig().getHeight() -3; rows++) {
@@ -65,27 +72,101 @@ public class UltimateWinner extends Player {
     return false;
   }
 
-  private boolean isTerminal(){
-    return false;
-  }
   private int minimaxChoice(Board board, int depth, boolean maximizingPlayer) {
     winningMove(board, Counter.O);
     winningMove(board, Counter.X);
-
-//    if (depth == 0 | isTerminal()) {
-//      if (isTerminal()) {
-//        if (winningMove(board, XPiece)) {
-//          return (None; 10000000000);
-//        }
-//        else if (winningMove(board, YPiece)) {
-//          return (None; -10000000000);
-//        }
-//        else {
-//          return (None; 0);
-//      }
-//    }
+    scorePosition(board, Counter.O);
+    scorePosition(board, Counter.X);
     return 5;
   }
+
+  public int evaluateWindow (Counter[] window, Counter counter) {
+    int score = 0;
+    Counter other = counter.getOther();
+    int playerCount = 0;
+    int otherCount = 0;
+    int emptyCount = 0;
+    for (Counter current : window){
+      if (current.getStringRepresentation() == counter.getStringRepresentation()){
+        playerCount++;
+      } else if (current.getStringRepresentation() == other.getStringRepresentation()) {
+        otherCount++;
+      } else {
+        emptyCount++;
+      }
+    }
+    if (playerCount == 4) {
+      score += 100;
+    } else if (playerCount == 3 && emptyCount == 1) {
+      score += 5;
+    } else if (playerCount == 2 && emptyCount == 2) {
+      score += 2;
+    }if (otherCount == 3 && emptyCount == 1) {
+      score -= 4;
+    }
+    return score;
+  }
+
+  //minute 15
+  public int scorePosition (Board board, Counter counter) {
+    int score = 0;
+
+    // Horizontal
+    for (int r = 0; r < board.getConfig().getHeight(); r++) {
+      //ArrayList row = new ArrayList();
+      for (int c = 0; c < board.getConfig().getWidth() - 3; c++) {
+        //row.add(board.getCounterAtPosition(new Position(r, c)));
+        Counter [] window = new Counter [4];
+        for (int i = 0; i < window.length; i++) {
+          window [i] = board.getCounterAtPosition(new Position(c+i, r));
+        }
+        score += evaluateWindow(window, counter);
+//        Counter [] window2 = {
+//                board.getCounterAtPosition(new Position(c, r)),
+//                board.getCounterAtPosition(new Position(c+1, r)),
+//                board.getCounterAtPosition(new Position(c+2, r)),
+//                board.getCounterAtPosition(new Position(c+3, r))
+        //};
+      }
+    }
+    // Vertical
+    for (int c = 0; c < board.getConfig().getWidth(); c++) {
+      for (int r = 0; r < board.getConfig().getHeight() - 3; r++) {
+        Counter[] window = new Counter[4];
+        for (int i = 0; i < window.length; i++) {
+          window[i] = board.getCounterAtPosition(new Position(c, r + i));
+        }
+        score += evaluateWindow(window, counter);
+      }
+    }
+
+    // Diagonal /
+    for (int c = 0; c < board.getConfig().getWidth() - 3; c++) {
+      for (int r = 0; r < board.getConfig().getHeight() - 3; r++) {
+        Counter[] window = new Counter[4];
+        for (int i = 0; i < window.length; i++) {
+          window[i] = board.getCounterAtPosition(new Position(c + i, r + i));
+        }
+        score += evaluateWindow(window, counter);
+      }
+    }
+
+    // Opposite Diagonal \
+    for (int c = 0; c < board.getConfig().getWidth() - 3; c++) {
+      for (int r = 0; r < board.getConfig().getHeight() - 3; r++) {
+        Counter[] window = new Counter[4];
+        for (int i = 0; i < window.length; i++) {
+          window[i] = board.getCounterAtPosition(new Position(c + 3 - i, r + i));
+        }
+        score += evaluateWindow(window, counter);
+      }
+    }
+    return score;
+  }
+
+
+
+
 
 }
 
