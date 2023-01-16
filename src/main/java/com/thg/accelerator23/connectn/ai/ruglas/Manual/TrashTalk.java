e2e9296a42438bba5d85ca57c543592cf8ed2d3e
Douglas Haigh <douglashaigh16@thehutgroup.com>
2023-01-16 09:51:38 +0000

    Added more trash talk

--- src/main/java/com/thg/accelerator23/connectn/ai/ruglas/Manual/TrashTalk.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/ruglas/Manual/TrashTalk.java
@@ -8,7 +8,20 @@ public class TrashTalk {
             "Did you set your minimax depth to 0 or something ?",
             "Did you get your AI from Wish ? ",
             "When mum says 'we've got an AI at home' she means yours.",
-            ""
+            "It's cute that you tried but embarrassing",
+            "I'd beat thee, but I would infect my hands.",
+            "Thou sodden-witted lord! Thou hast no more brain than I have in mine elbows ",
+            "Thine face is not worth sunburning." ,
+            "Thy tongue outvenoms all the worms of Nile.",
+            "Thy bot is as dry as the remainder biscuit after voyage.",
+            "Heaven truly knows that thou art false as hell",
+            "Thine algorithm truly is clapped.",
+            "Shall I compare thee to a rainy day in Manchester ?",
+            "Get to the bottom of the stack, and there remain.",
+            "Go home algorithm, you're drunk.",
+
+
+
     };
 
     public static void talkTrash() {
