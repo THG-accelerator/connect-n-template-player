227efba7d2d58e3640c9179e9db30931e28a515f
Luca Tonti <luca.tonti52@thethutgroup.com>
2024-01-22 11:42:07 +0000

    First commit, random AI

--- src/main/java/com/thg/accelerator23/connectn/ai/luca/PleaseDontFailAi.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/luca/PleaseDontFailAi.java
@@ -1,20 +1,23 @@
-package com.thg.accelerator23.connectn.ai.yourteam;
+package com.thg.accelerator23.connectn.ai.luca;
 
 import com.thehutgroup.accelerator.connectn.player.Board;
 import com.thehutgroup.accelerator.connectn.player.Counter;
 import com.thehutgroup.accelerator.connectn.player.Player;
 
+// ADDED LIBRARIES
+import java.util.Random;
 
-public class NameAi extends Player {
-  public NameAi(Counter counter) {
+
+public class PleaseDontFailAi extends Player {
+  public PleaseDontFailAi(Counter counter) {
     //TODO: fill in your name here
-    super(counter, NameAi.class.getName());
+    super(counter, PleaseDontFailAi.class.getName());
   }
 
   @Override
   public int makeMove(Board board) {
     //TODO: some crazy analysis
     //TODO: make sure said analysis uses less than 2G of heap and returns within 10 seconds on whichever machine is running it
-    return 4;
+    return new Random().nextInt(board.getConfig().getWidth());
   }
 }
