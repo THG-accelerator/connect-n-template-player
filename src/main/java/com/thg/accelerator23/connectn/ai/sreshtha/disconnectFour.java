498a862a050e4688a4b80afb944c5e354679f092
Gulfam Mahmud <gulfam.mahmud29@thehutgroup.com>
2024-01-22 14:48:44 +0000

    setup

--- src/main/java/com/thg/accelerator23/connectn/ai/sreshtha/disconnectFour.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/sreshtha/disconnectFour.java
@@ -1,14 +1,14 @@
-package com.thg.accelerator23.connectn.ai.yourteam;
+package com.thg.accelerator23.connectn.ai.sreshtha;
 
 import com.thehutgroup.accelerator.connectn.player.Board;
 import com.thehutgroup.accelerator.connectn.player.Counter;
 import com.thehutgroup.accelerator.connectn.player.Player;
 
 
-public class NameAi extends Player {
-  public NameAi(Counter counter) {
+public class disconnectFour extends Player {
+  public disconnectFour(Counter counter) {
     //TODO: fill in your name here
-    super(counter, NameAi.class.getName());
+    super(counter, disconnectFour.class.getName());
   }
 
   @Override
