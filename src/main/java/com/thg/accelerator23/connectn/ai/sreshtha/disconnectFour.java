ad495c497690a745a0337fd09a0f7da3028b9c11
Gulfam Mahmud <gulfam.mahmud29@thehutgroup.com>
2024-01-24 09:38:39 +0000

    setup again?

--- src/main/java/com/thg/accelerator23/connectn/ai/sreshtha/disconnectFour.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/sreshtha/disconnectFour.java
@@ -7,14 +7,15 @@ import com.thehutgroup.accelerator.connectn.player.Player;
 
 public class disconnectFour extends Player {
   public disconnectFour(Counter counter) {
-    //TODO: fill in your name here
     super(counter, disconnectFour.class.getName());
   }
 
   @Override
   public int makeMove(Board board) {
+    System.out.println("Hello World!");
     //TODO: some crazy analysis
     //TODO: make sure said analysis uses less than 2G of heap and returns within 10 seconds on whichever machine is running it
     return 4;
   }
+
 }
