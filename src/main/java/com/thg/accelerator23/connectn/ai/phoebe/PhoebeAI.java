a9650a1677011d2f05509941ba478755fe2816b4
Phoebe Evans <Phoebe.Evans19@thehutgroup.com>
2024-01-24 11:58:59 +0000

    edited yourteam error

--- src/main/java/com/thg/accelerator23/connectn/ai/phoebe/PhoebeAI.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/phoebe/PhoebeAI.java
@@ -1,14 +1,14 @@
-package com.thg.accelerator23.connectn.ai.yourteam;
+package com.thg.accelerator23.connectn.ai.connectn;
 
 import com.thehutgroup.accelerator.connectn.player.Board;
 import com.thehutgroup.accelerator.connectn.player.Counter;
 import com.thehutgroup.accelerator.connectn.player.Player;
 
 
-public class NameAi extends Player {
-  public NameAi(Counter counter) {
+public class ConnectN extends Player {
+  public ConnectN(Counter counter) {
     //TODO: fill in your name here
-    super(counter, NameAi.class.getName());
+    super(counter, ConnectN.class.getName());
   }
 
   @Override
