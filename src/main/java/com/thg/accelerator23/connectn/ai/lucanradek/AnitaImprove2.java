52dd480a2c982b9c66fab0c25674cd5c83441d0f
skaroupkaTHG <radek.skaroupka67@thehutgroup.com>
2024-01-25 16:20:17 +0000

    commit before leaving

--- src/main/java/com/thg/accelerator23/connectn/ai/lucanradek/AnitaImprove2.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/lucanradek/AnitaImprove2.java
@@ -14,66 +14,22 @@ import java.util.Map;
 
 public class AnitaImprove2 extends Player {
 
-    byte[] myCounters = new byte[80];
-    byte[] theirCounters = new byte[80];
-
-    Int128 longInt = 223;
-
-
+    public class ByteBoard {
+        byte[] byteCounters;
+        public ByteBoard() {:
+            this.byteCounters = new byte[10]{0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000,
+                    0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000};
+        }
+    }
     public AnitaImprove2(Counter counter) {
-        // Anita Improve, created by Radek
+        // Anita Improvise, created by Radek
         super(counter, AnitaImprove2.class.getName());
     }
 
-    protected int convertPositionToIndex(Position position) {
-        return position.getX() * 10 + position.getY();
-    }
-
-
-    protected byte[] addPosition(byte[] positions, Position position) {
-        positions[convertPositionToIndex(position)] = 1;
-        return positions;
-    }
-
-    protected void updateTheirCounters(Position position) {
-        this.theirCounters = addPosition(this.theirCounters, position);
-    }
-
-    protected void updateMyCounters(Position position) {
-        this.myCounters = addPosition(this.myCounters, position);
-    }
-
-    protected boolean isPositionEmpty(Position position) {
-        int index = convertPositionToIndex(position);
-        return myCounters[index] == 0 || theirCounters[index] == 0;
-    }
-
-    protected void findTheirCounters(Board board) {
-        for (int row = 0; row < board.getConfig().getWidth(); row++) {
-            for (int col = 0; col < board.getConfig().getHeight(); col++) {
-                Position tempPosition = new Position(row, col);
-                boolean canUpdateTheirCounters = board.hasCounterAtPosition(tempPosition)
-                        && !board.getCounterAtPosition(tempPosition).equals(this.getCounter())
-                        && !this.isPositionEmpty(tempPosition);
-                if (canUpdateTheirCounters) {
-                    updateTheirCounters(tempPosition);
-                    return;
-                }
-            }
-        }
-    }
 
 
     @Override
     public int makeMove(Board board) {
-        findTheirCounters(board);
-
-
-        int x = 1;
-        int y = 2;
-
-
-        updateMyCounters(new Position(x, y));
-        return y;
+        return 4;
     }
 }
