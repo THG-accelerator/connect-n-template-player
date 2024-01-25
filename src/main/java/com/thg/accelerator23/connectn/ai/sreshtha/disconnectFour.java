c37a1d9e42aa87c0ba1587e957f2106bed08c1d5
Gulfam Mahmud <gulfam.mahmud29@thehutgroup.com>
2024-01-25 23:00:47 +0000

    minimax maybe works

--- src/main/java/com/thg/accelerator23/connectn/ai/sreshtha/disconnectFour.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/sreshtha/disconnectFour.java
@@ -1,9 +1,7 @@
 package com.thg.accelerator23.connectn.ai.sreshtha;
 
-import com.thehutgroup.accelerator.connectn.player.Board;
-import com.thehutgroup.accelerator.connectn.player.Counter;
-import com.thehutgroup.accelerator.connectn.player.InvalidMoveException;
-import com.thehutgroup.accelerator.connectn.player.Player;
+import com.thehutgroup.accelerator.connectn.player.*;
+import com.thg.accelerator23.connectn.ai.sreshtha.analysis.BoardAnalyser;
 import com.thg.accelerator23.connectn.ai.sreshtha.analysis.GameState;
 
 
@@ -14,39 +12,53 @@ public class disconnectFour extends Player {
 
   @Override
   public int makeMove(Board board) {
-    int board_width = 10;
-    int board_height = 8;
+//    int board_width = 10;
+//    int board_height = 8;
+//
+//    int min = 0;
+//    int max = board_width - 1;
+//
+//    int random = (int)Math.floor(Math.random() * (max - min + 1) + min);
+//    //TODO: some crazy analysis
+//    //TODO: make sure said analysis uses less than 2G of heap and returns within 10 seconds on whichever machine is running it
+//    return random;
 
-    int min = 0;
-    int max = board_width - 1;
 
-    int random = (int)Math.floor(Math.random() * (max - min + 1) + min);
-    //TODO: some crazy analysis
-    //TODO: make sure said analysis uses less than 2G of heap and returns within 10 seconds on whichever machine is running it
-    return random;
-  }
+    int bestScore = Integer.MIN_VALUE;
+    int depth = 7;
+    int bestMove=-1;
 
-  public int eval(GameState gameState){
-    Counter counter = getCounter();
-    Counter winner = gameState.getWinner();
-    if(winner==counter){
-      return 1;
+    for(int i=0; i < board.getConfig().getWidth(); i++){
+        try {
+            Board newBoard = new Board(board, i, getCounter());
+            int score = minimax(newBoard, depth, Integer.MIN_VALUE, Integer.MAX_VALUE, false);
+            if(score > bestScore){
+              bestScore = score;
+              bestMove = i;
+            }
+        } catch (InvalidMoveException e) {
+            throw new RuntimeException(e);
+        }
     }
-    return 0;
+
+    return bestMove;
   }
+
 
   // let's try minimax, dunno if it will work :)
   // alpha beta pruning removes the depth of the minimax algorithm, this allows it to take up less tree memory therefore faster : )
-  public int minimax(Board board, int depth, int alpha, int beta, boolean maximisingPlayer, GameState gameState) throws InvalidMoveException {
+  public int minimax(Board board, int depth, int alpha, int beta, boolean maximisingPlayer) throws InvalidMoveException {
+    GameState gameState = new BoardAnalyser(board.getConfig()).calculateGameState(board);
+
     if(depth==0 || gameState.isWin() || gameState.isEnd()){
-      return eval(gameState);
+      return evaluate(board);
     }
 
     if(maximisingPlayer){
       double maxEval = Double.NEGATIVE_INFINITY;
       for(int i=0; i < board.getConfig().getWidth(); i++){
         Board b = new Board(board, i, this.getCounter());
-        int new_score = minimax(b, depth-1, alpha, beta, false, gameState);
+        int new_score = minimax(b, depth-1, alpha, beta, false);
         if(new_score > maxEval){
           maxEval = new_score;
           alpha = (int)Math.max(alpha, maxEval);
@@ -60,7 +72,7 @@ public class disconnectFour extends Player {
       double minEval = Double.POSITIVE_INFINITY;
       for(int i=0; i< board.getConfig().getWidth(); i++){
         Board bo = new Board(board, i, this.getCounter().getOther());
-        int new_score = minimax(board, depth-1, alpha, beta, true, gameState);
+        int new_score = minimax(bo, depth-1, alpha, beta, true);
         minEval = Math.max(minEval, new_score);
         beta = (int)Math.min(beta, minEval);
         if(alpha >= beta){
@@ -69,6 +81,72 @@ public class disconnectFour extends Player {
       }return (int)minEval;
     }
 
+  }
+
+  public int calcScorePos(Board board, int row, int col, int increment_row, int increment_col){
+    int maxingPoints = 0, miningPoints = 0;
+    for(int i=0; i<4; i++){
+      if(board.getCounterAtPosition(new Position(col, row)) == Counter.X){
+        maxingPoints++;
+      }else if(board.getCounterAtPosition(new Position(col, row)) == Counter.O){
+        miningPoints++;
+      }
+      row += increment_row;
+      col += increment_col;
+    }
+    if(maxingPoints == 4){
+      return Integer.MAX_VALUE;
+    }else if(miningPoints == 4){
+      return Integer.MIN_VALUE; // change this
+    }else {
+      return maxingPoints;
+    }
+  }
+
+  public int evaluate(Board board) {
+    int verticalPoints = 0, horizontalPoints = 0, decsDiagPoint = 0, ascDiagPoints = 0, totalPoints = 0;
+    for (int row = 0; row < board.getConfig().getHeight() - 3; row++) {
+      for (int col = 0; col < board.getConfig().getWidth(); col++) {
+        int tempScore = calcScorePos(board, row, col, 1, 0);
+        verticalPoints += tempScore;
+        if (tempScore >= Integer.MAX_VALUE || tempScore <= Integer.MIN_VALUE) {
+          return tempScore;
+        }
+
+      }
+    }
+    for (int row = 0; row < board.getConfig().getHeight(); row++) {
+      for (int col = 0; col < board.getConfig().getWidth() - 3; col++) {
+        int tempScore = calcScorePos(board, row, col, 0, 1);
+        horizontalPoints += tempScore;
+        if (tempScore >= Integer.MAX_VALUE || tempScore <= Integer.MIN_VALUE) {
+          return tempScore;
+        }
+
+      }
+    }
+
+    for(int row=0; row< board.getConfig().getHeight() - 3; row++){ //may need to change the -3 and the -4
+      for(int col=0; col < board.getConfig().getHeight() - 3; col++){
+        int tempScore = calcScorePos(board, row, col, 1, 1);
+        decsDiagPoint += tempScore;
+        if(tempScore>=Integer.MAX_VALUE || tempScore <= Integer.MIN_VALUE){
+          return tempScore;
+        }
+      }
+    }
+    for(int row=3; row< board.getConfig().getHeight(); row++){
+      for(int col=0; col<board.getConfig().getHeight() - 4; col++){
+        int tempScore = calcScorePos(board, row, col, -1, 1);
+        ascDiagPoints += tempScore;
+        if(tempScore >= Integer.MAX_VALUE || tempScore <= Integer.MIN_VALUE){
+          return tempScore;
+        }
+      }
+    }
+
+    totalPoints = verticalPoints + horizontalPoints + decsDiagPoint + ascDiagPoints;
+    return totalPoints;
 
   }
 
