d925e3c56cf0a28ff8faf9df1f9bed122a1779c0
Joseph Lin <Joseph.Lin10@thg.com>
2025-01-20 11:53:59 +0000

    set up timer

--- src/main/java/com/thg/accelerator23/connectn/ai/onleave/onLeaveSlowResponse.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/onleave/onLeaveSlowResponse.java
@@ -4,8 +4,19 @@ import com.thehutgroup.accelerator.connectn.player.Board;
 import com.thehutgroup.accelerator.connectn.player.Counter;
 import com.thehutgroup.accelerator.connectn.player.Player;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class onLeaveSlowResponse extends Player {
+
+  // timeout value, gives 500ms buffer
+  private static final long TIMEOUT_SECONDS = 9500;
+
+  private int bestMoveFound;
+  private int bestScoreFound;
+  private long startTime;
+
   public onLeaveSlowResponse(Counter counter) {
     //TODO: fill in your name here
     super(counter, onLeaveSlowResponse.class.getName());
@@ -15,6 +26,45 @@ public class onLeaveSlowResponse extends Player {
   public int makeMove(Board board) {
     //TODO: some crazy analysis
     //TODO: make sure said analysis uses less than 2G of heap and returns within 10 seconds on whichever machine is running it
-    return 4;
+    startTime = System.currentTimeMillis();
+    bestMoveFound = 4;
+    bestScoreFound = Integer.MIN_VALUE;
+
+    List<Integer> legalMoves = getLegalMoves(board);
+
+    for (int depth = 1; !isTimeUp(); depth++) {
+      try {
+        searchMovesAtDepth(board, depth, legalMoves);
+      } catch (TimeoutException e) {
+        break;
+      }
+    }
+
+    return bestMoveFound;
   }
+
+  private List<Integer> getLegalMoves(Board board) {
+    return new ArrayList<>();
+  }
+
+  private void searchMovesAtDepth(Board board, int depth, List<Integer> legalMoves) throws TimeoutException {
+    for (int move : legalMoves) {
+      if (isTimeUp()) {
+        throw new TimeoutException();
+      }
+      // do minimax
+      int score = 0;
+
+      if (score > bestScoreFound) {
+        bestScoreFound = score;
+        bestMoveFound = move;
+      }
+    }
+  }
+
+  private boolean isTimeUp() {
+    return System.currentTimeMillis() - startTime > TIMEOUT_SECONDS;
+  }
+
+  private static class TimeoutException extends Exception {}
 }
