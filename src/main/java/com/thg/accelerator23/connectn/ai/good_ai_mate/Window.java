8783574b0df1dcf8a81e2a8a977039f1527eefc2
DavidWhitlock695 <David.Whitlock27@thg.com>
2025-01-22 09:19:20 +0000

    shaan-window-work

--- src/main/java/com/thg/accelerator23/connectn/ai/good_ai_mate/Window.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/good_ai_mate/Window.java
@@ -1,7 +1,5 @@
 package com.thg.accelerator23.connectn.ai.good_ai_mate;
-
 import com.thehutgroup.accelerator.connectn.player.Counter;
-
 public class Window {
     private int score;
     private final Counter[] array = new Counter[4];
@@ -13,9 +11,35 @@ public class Window {
         this.array[3] = d;
     }
 
-    public int getWindowScore(){
+    public int getWindowScore(Counter botPiece, Counter emptyPiece){
         //TODO logic for calculating score
-        return 1;
-    }
+        int score = 0;
+        int botPieceCount = 0;
+        int emptyPieceCount = 0;
+        int oppPieceCount = 0;
+        Counter oppPiece = botPiece.getOther();
 
+        // Iterating over each counter in window
+        for (Counter c : array) {
+            if (c == botPiece) {
+                botPieceCount++;
+            } else if (c == emptyPiece) {
+                emptyPieceCount++;
+            } else {
+                oppPieceCount++;
+            }
+        }
+
+        // Scoring logic
+        if (botPieceCount == 4) {
+            score += 100;
+        } else if (botPieceCount == 3 && emptyPieceCount == 1) {
+            score += 5;
+        } else if (botPieceCount == 2 && emptyPieceCount == 2) {
+            score += 2;
+        } else if (oppPieceCount == 3 && emptyPieceCount == 1) {
+            score -= 4;
+        }
+        return score;
+    }
 }
